13659dfa4363a3c3140993c51c9710deb50ba399
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-21 14:11:25 +0530

    Update CatRepository.java

--- main/entity/Repository/CatRepository.java
+++ main/entity/Repository/CatRepository.java
@@ -1,1 +1,8 @@
+package com.example.petapi.repository;
+
+import com.example.petapi.model.Cat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CatRepository extends JpaRepository<Cat, Long> {
+}
 
