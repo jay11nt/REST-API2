898ca4e62ba31d62ce1fab337d8aa4689105bf30
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-21 14:17:14 +0530

    Update DogRepository.java

--- main/Repository/DogRepository.java
+++ main/Repository/DogRepository.java
@@ -1,1 +1,8 @@
+package com.example.petapi.repository;
+
+import com.example.petapi.model.Dog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DogRepository extends JpaRepository<Dog, Long> {
+}
 
