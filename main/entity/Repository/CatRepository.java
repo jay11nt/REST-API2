6f6b73b69978db87de779f993e7f230d9f5cddaf
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-21 14:14:26 +0530

    Delete main/entity/Repository directory

--- main/entity/Repository/CatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.petapi.repository;
-
-import com.example.petapi.model.Cat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatRepository extends JpaRepository<Cat, Long> {
-}
-
--- main/entity/Repository/DogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.petapi.repository;
-
-import com.example.petapi.model.Dog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DogRepository extends JpaRepository<Dog, Long> {
-}
-
