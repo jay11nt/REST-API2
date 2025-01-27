2d84fa4766e6b2b2f139aecf9ee4e962a7b9845d
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-27 09:25:33 +0530

    Update Dog.java

--- main/entity/Dog.java
+++ main/entity/Dog.java
@@ -15,14 +15,14 @@ public class Dog
     // Getters and Setters
 
     public Long getId() 
-    {
+        {
         return id;
-    }
+        }
 
     public void setId(Long id) 
-    {
+        {
         this.id = id;
-    }
+        }
 
     public String getName() 
     {
